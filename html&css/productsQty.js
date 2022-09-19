87ea63fc805b131bd9a3f478d438ce731dd75f83
mamaruf04 <mamar@DESKTOP-AR6EM10.(none)>
2022-09-19 13:20:35 +0600

    some function minimul done

--- html&css/productsQty.js
+++ html&css/productsQty.js
@@ -3,33 +3,35 @@ const productInput = document.getElementById("productInput1");
 // product price 
 const productPrice = document.getElementById("productPrice");
 
+// increase & decrease function.
+const counter = (isIncrease) =>{
+    let productInputValue = productInput.value;
+    if(isIncrease){
+        productInputValue ++;
+    } 
+    else if (!isIncrease && productInputValue > 1) {
+        productInputValue --;
+    }
+    else{
+        alert("please add some quantities.");
+    }
+    productInput.value = productInputValue;
+    // product price based on quantity.
+    let totalPrice = productInputValue * 1150;
+    productPrice.innerText = totalPrice;
+}
+
 
 // minus button.
 document.getElementById("ProductMinus1").addEventListener('click', () => {
 
-    let productInputValue = productInput.value;
-    if(productInputValue > 0){
-        productInputValue --;
-    } 
-    else alert("please add some quantities.");
-    productInput.value = productInputValue;
+    counter(false);
 
-    // product price based on quantity.
-    let totalPrice = productInputValue * 1150;
-    productPrice.innerText = totalPrice;
-})
+});
 
 // plus button.
 document.getElementById("ProductPlus1").addEventListener('click', () => {
 
-    let productInputValue = productInput.value;
-    productInputValue ++;
-    productInput.value = productInputValue;
+    counter(true);
 
-    // product price based on quantity.
-    let totalPrice = productInputValue * 1150;
-    productPrice.innerText = totalPrice;
-})
-
-
-document.getElementById("")
+});
